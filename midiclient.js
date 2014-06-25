9d3c33fc61b59f93aa860547ebecc605aa8dc065
mars <martin@rebel-it.com>
2014-06-25 13:01:47 +0100

    interpret Sysex parameters and patchnames

--- midiclient.js
+++ midiclient.js
@@ -20,8 +20,13 @@ function onMIDIInit(midi, options) {
 	alert("No MIDI output devices present.")
     else {
 	for (var i=0;i<outputs.length;i++){
-	    midiOutputs.push(outputs[i]);
-	    console.log("added MIDI output "+outputs[i].name+" ("+outputs[i].manufacturer+") "+outputs[i].id);
+	    console.log("adding MIDI output "+i+"/"+outputs.length);
+	    try{
+		midiOutputs.push(outputs[i]);
+		console.log("added MIDI output "+outputs[i].name+" ("+outputs[i].manufacturer+") "+outputs[i].id);
+	    }catch(exc){
+		console.log("MIDI output "+i+" error: "+exc);
+	    }
 	}
     }
 }
@@ -30,16 +35,15 @@ function onMIDIReject(err) {
     alert("The MIDI system failed to start.");
 }
 
+var sysexMessage = [];
 function MIDIMessageEventHandler(event) {
-    console.log("received "+event.data.length+" bytes 0x"+event.data[0].toString(16));
-    // Mask off the lower nibble (MIDI channel, which we don't care about)
-    switch (event.data[0] & 0xf0) {
+    console.log("MIDI 0x"+event.data[0].toString(16)+" "+event.data.length+" bytes");
+    switch(event.data[0] & 0xf0) {
     case 0x90:
-	if (event.data[2]!=0) {  // if velocity != 0, this is a note-on message
+	if (event.data[2] != 0) {  // if velocity != 0, this is a note-on message
 	    noteOn(event.data[1], event.data[2]);
 	    return;
 	}
-	// if velocity == 0, fall thru: it's a note-off.  MIDI's weird, ya'll.
     case 0x80:
 	noteOff(event.data[1]);
 	return;
@@ -47,8 +51,14 @@ function MIDIMessageEventHandler(event) {
 	controlChange(event.data[1], event.data[2]);
 	return;
     case 0xF0:
-	console.log("sysex");
-	systemExclusive(event.data);
+  	systemExclusive(event.data);
+	// sysexMessage = sysexMessage.concat(event.data);
+	// console.log("sysex evt 0x"+event.data[0].toString(16)+":0x"+event.data[event.data.length-1].toString(16));
+	// console.log("sysex msg 0x"+sysexMessage[0].toString(16)+":0x"+sysexMessage[sysexMessage.length-1].toString(16));
+	// if(sysexMessage.indexOf(0xf7) != -1){
+  	//     systemExclusive(sysexMessage);
+	//     sysexMessage = [];
+	// }
 	return;
     }
 }
