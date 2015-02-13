9b17452a58647ae9c9a25f78bc1d3bd5a9dee4c3
samuele-artuso <sam@highoctanedev.co.uk>
2015-02-13 18:51:36 +0000

    DB update: allowing multiple GitHub files.

--- web/mongo/db-migrations/001.js
+++ web/mongo/db-migrations/001.js
@@ -9,3 +9,23 @@ db.patches.find({ soundcloud: { $exists: true }}).forEach(function(doc) {
 /*
  * Allowing multiple GitHub files instead of one
  */
+db.patches.find({ github: { $exists: true }}).forEach(function(doc) {
+    doc.github = [ doc.github ];
+    db.patches.save(doc);
+});
+
+/*
+ * Changing format of GitHb files
+ */
+db.patches.find({ github: { $exists: true }}).forEach(function(doc) {
+    var file = 'https://github.com/' + doc.repo + '/blob/master/' + doc.github[0];
+    doc.github = [ file ];
+    db.patches.save(doc);
+});
+
+/*
+ * Delete old repository information
+ * 
+ * RUN AS A SEPARATE STEP!!!
+ */
+db.patches.update({}, { $unset: { repo: "" }}, { multi: true });
