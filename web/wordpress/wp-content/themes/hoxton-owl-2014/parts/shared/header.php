c7cef4960bcf3beacf39d53aa1873ca3d2164817
samuele-artuso <sam@highoctanedev.co.uk>
2015-03-12 16:12:57 +0000

    Adding in "log in" and "register" links in top nav. (fixes #55)

--- web/wordpress/wp-content/themes/hoxton-owl-2014/parts/shared/header.php
+++ web/wordpress/wp-content/themes/hoxton-owl-2014/parts/shared/header.php
@@ -4,11 +4,25 @@
     <div id="primary-nav-container">
         <a href="<?php echo get_option('home'); ?>"><img id="logo" src="<?php echo get_bloginfo('template_directory');?>/images/logo.png" alt="Hoxton Owl"></a>
         <ul id="primary-nav-items">
-            <?php wp_nav_menu( array( 'container_class' => 'main-nav', 'theme_location' => 'primary' ) ); ?>
+            <?php wp_nav_menu(array('container_class' => 'main-nav', 'theme_location' => 'primary')); ?>
         </ul>
         <ul id="reg-signin-items">
-            <!--<li><a href="#">Register</a></li>
-            <li><a href="#">Sign in</a></li>-->
+<?php
+
+if (!is_user_logged_in()) {
+?>
+            <li><a href="<?php echo wp_registration_url(); ?>">Register</a></li>
+            <li><a href="<?php echo wp_login_url( $redirect ); ?>">Sign In</a></li>
+<?php
+
+} else {
+    $currentUser = wp_get_current_user();
+?>
+            <li><a href="/forums/users/<?php echo htmlspecialchars($currentUser->user_login); ?>/edit/"><?php echo htmlspecialchars($currentUser->user_login); ?></a></li>
+            <li><a href="<?php echo wp_logout_url(); ?>">Log Out</a></li>
+<?php
+}
+?>
             <li><a href="http://www.rebeltech.org/shop/" class="order-button">Order</a></li>
         </ul>
     </div>
