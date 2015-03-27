985343a5caf9824b2ba739a662c0bffd2a94cef0
samuele-artuso <sam@highoctanedev.co.uk>
2015-03-27 18:03:13 +0000

    Patch builder: adding separate settings file.

---
+++ web/scripts/patch-builder/_meta/settings.tpl.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author Sam Artuso <sam@highoctanedev.co.uk>
+ */
+
+define('MONGO_HOST',       'localhost');
+define('MONGO_PORT',       27017);
+define('MONGO_USE_AUTH',   false);
+define('MONGO_USER',       '');
+define('MONGO_PASS',       '');
+define('MONGO_DATABASE',   'owl_staging');
+define('MONGO_COLLECTION', 'patches');
