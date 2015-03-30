d322d2d6cfd6013c0863ffffff8f44fd0f015428
samuele-artuso <sam@highoctanedev.co.uk>
2015-03-30 18:02:45 +0100

    Online compilation - backend (#66).

--- web/api/routes/sysex.js
+++ web/api/routes/sysex.js
@@ -6,6 +6,7 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 var path = require('path');
+var exec = require('child_process').exec;
 var Q = require('q');
 Q.longStackSupport = false; // To be enabled only when debugging
 
@@ -66,6 +67,38 @@ router.get('/:id', function(req, res) {
             });
         }
     );
+});
+
+/**
+ * Recompiles a patch.
+ *
+ * PUT /patch/{id}
+ */
+router.put('/:id', function(req, res) {
+
+    var id = req.params.id;
+    if (!/^[a-f\d]{24}$/i.test(id)) {
+        return res.status(500).json({
+           message: 'Invalid ID.',
+           error: { status: 500 }
+        });
+    }
+
+    var cmd = 'php ' + apiSettings.PATCH_BUILDER_PATH + ' ' + id;
+
+    exec(cmd, function (error, stdout, stderr) {
+
+        var response = {
+            stdout: stdout,
+            stderr: stderr,
+            success: error === null
+        };
+        if (error !== null) {
+            response.error = { status: 500 };
+        }
+        return res.status(error !== null ? 500 : 200).json(response);
+
+    });
 });
 
 module.exports = router;
