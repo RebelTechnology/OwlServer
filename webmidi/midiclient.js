ffc7bc8d1c6b901397afdef22774686b17655008
mars <martin@rebel-it.com>
2015-03-30 15:45:25 +0100

    updated to latest WebMIDIAPI.js

--- webmidi/midiclient.js
+++ webmidi/midiclient.js
@@ -3,30 +3,48 @@ var midiOutputs = [];
 var midiOutput = null;
 
 function onMIDIInit(midi, options) {
-    // console.log("MIDI sysex options: "+options);
-    // console.log("MIDI sysex: "+midi.sysexEnabled);
+    console.log("MIDI sysex options: "+options);
+    console.log("MIDI sysex: "+midi.sysexEnabled);
     midiAccess = midi;
 
-    var inputs=midiAccess.inputs();
-    if (inputs.length === 0)
-	console.log("No MIDI input devices present.")
-    else { // Hook the message handler for all MIDI inputs
-	for (var i=0;i<inputs.length;i++){
-	    inputs[i].onmidimessage = MIDIMessageEventHandler;
-	    console.log("added MIDI input "+inputs[i].name+" ("+inputs[i].manufacturer+") "+inputs[i].id);
-	}
+    var i = 0;
+    var iterator = midiAccess.inputs.values();
+    var data;
+    while((data = iterator.next()).done === false){
+        port = data.value;
+        console.log( i++ + ": " + port.name + "; manufacturer: " + port.manufacturer + "; version: " + port.version + "\n");
     }
-    var outputs=midiAccess.outputs();
-    if (outputs.length === 0)
+
+    var inputs = midiAccess.inputs.values();
+    if(inputs.size === 0)
+    	console.log("No MIDI input devices present.")
+    for(var input = inputs.next(); input && !input.done; input = inputs.next()) {
+	input.value.onmidimessage = MIDIMessageEventHandler;
+	console.log("added MIDI input "+input.value.name+" ("+input.value.manufacturer+") "+input.value.id);
+    }
+
+    // var inputs = midiAccess.inputs.values();
+    // if(inputs.size === 0)
+    // 	console.log("No MIDI input devices present.")
+    // else { // Hook the message handler for all MIDI inputs
+    // 	for (var i=0;i<inputs.size;i++){
+    // 	    inputs.values()[i].onmidimessage = MIDIMessageEventHandler;
+    // 	    console.log("added MIDI input "+inputs[i].name+" ("+inputs[i].manufacturer+") "+inputs[i].id);
+    // 	}
+    // }
+    var outputs = midiAccess.outputs.values();
+    var i = 0;
+    for(var output = outputs.next(); output && !output.done; output = outputs.next()) {
+        midiOutputs.push(output.value);
+	registerMidiOutput(i++, output.value.name);
+	console.log("added MIDI output "+output.value.name+" ("+output.value.manufacturer+") "+output.value.id);
+    }
+    if(outputs.size === 0)
 	console.log("No MIDI output devices present.")
-    else {
-	for (var i=0;i<outputs.length;i++){
-            midiOutputs.push(outputs[i]);
-	    registerMidiOutput(i, outputs[i].name);
-	    console.log("added MIDI output "+outputs[i].name+" ("+outputs[i].manufacturer+") "+outputs[i].id);
-	}
-        midiOutput = outputs[outputs.length - 1];
-    }
+    else
+	selectMidiOutput(0);
+    // midiOutput = midiOutputs[midiOutputs.size - 1];
+    // midiOutput = outputs[outputs.size - 1];
 }
 
 function onMIDIReject(err) {
@@ -35,7 +53,7 @@ function onMIDIReject(err) {
 
 var sysexMessage = [];
 function MIDIMessageEventHandler(event) {
-    // console.log("MIDI 0x"+event.data[0].toString(16)+" "+event.data.length+" bytes");
+    console.log("MIDI 0x"+event.data[0].toString(16)+" "+event.data.length+" bytes");
     switch(event.data[0] & 0xf0) {
     case 0x90:
 	if(event.data[2] != 0) {  // if velocity != 0, this is a note-on message
@@ -68,7 +86,7 @@ function selectMidiOutput(index) {
 }
 
 function sendCc(cc, value) {
-    // console.log("sending CC "+cc+"/"+value);
+    console.log("sending CC "+cc+"/"+value);
     if(midiOutput)
       midiOutput.send([0xB0, cc, value], 0);
 }
@@ -79,7 +97,7 @@ window.addEventListener('load', function() {
     context = new AudioContext();
     var options = { sysex: true };
     if (navigator.requestMIDIAccess)
-	navigator.requestMIDIAccess(options).then( onMIDIInit, onMIDIReject );
+	navigator.requestMIDIAccess( { sysex: true } ).then( onMIDIInit, onMIDIReject );
     else
 	alert("No MIDI support present in your browser.")
 } );
