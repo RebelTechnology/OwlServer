d2f223788d21dd470dbae8cb04101768b4f0b55d
samuele-artuso <sam@highoctanedev.co.uk>
2015-11-27 18:57:25 +0000

    Refactoring GET /authors

--- web/api/routes/authors.js
+++ web/api/routes/authors.js
@@ -4,6 +4,10 @@
 
 var express = require('express');
 var router = express.Router();
+var Q = require('q');
+
+var wordpressBridge = require('../lib/wordpress-bridge.js');
+var apiSettings     = require('../api-settings.js');
 
 /**
  * Retrieves all authors.
@@ -12,31 +16,116 @@ var router = express.Router();
  */
 router.get('/', function(req, res) {
 
+    /*
+     * Authors can either be:
+     * * WordPress users, in which case they are uniquely identified by their
+     *   WordPress user ID.
+     * * Other users, in which case they are uniquely identified by their name.
+     *
+     * All that is stored in the database about WordPress users is their
+     * WordPress user ID.
+     */
+
     var collection = req.db.get('patches');
-    var nativeCol = collection.col;
-    nativeCol.aggregate(
+    var authorWpIds = []; // an array whose values are WordPress user IDs
+    var authorNames = {}; // an object whose keys are non-WP author names
+    var result = {
+        count: 0,
+        result: []
+    };
 
-        { $project: { "author": 1, lowercase: { $toLower: "$author.name" }}},
-        { $group: { _id: "$author.name", url: { $first: "$author.url" }, lowercase: { $first: "$lowercase" }}},
-        { $sort: { lowercase: 1 }},
-        { $project: { _id: "$_id", url: "$url" }},
+    var getUserInfoBatch = Q.denodeify(wordpressBridge.getUserInfoBatch);
 
-        function(err, result) {
-            if (err !== null) {
-                return res.status(500).json({message: err});
+    Q.fcall(function () {
+
+        /*
+         * Get all patches
+         */
+
+        return collection.find({});
+
+    }).then(function (docs) {
+
+        for (var i = 0, max = docs.length; i < max; i++) {
+            if (docs[i].author.wordpressId) {
+                authorWpIds.push(docs[i].author.wordpressId);
             } else {
-                var authors = [];
-                for (var i = 0; i < result.length; i++) {
-                    var author = { name: result[i]._id };
-                    if (null !== result[i].url) {
-                        author.url = result[i].url;
-                    }
-                    authors.push(author);
-                }
-                return res.status(200).json({ count: authors.length, result: authors });
+                authorNames[docs[i].author.name] = true;
             }
         }
-    );
+
+        /*
+         * Add all authors whose name we know to the final result
+         */
+
+        for (name in authorNames) {
+            result.result.push({ name: name });
+            result.count++;
+        }
+
+        /*
+         * Remove duplicate WP IDs
+         */
+
+        authorWpIds = authorWpIds.reduce(function (p, c) {
+            if (p.indexOf(c) < 0) {
+                p.push(c);
+            }
+            return p;
+        }, []);
+
+        /*
+         * Get user "display names" from WordPress
+         */
+
+        return getUserInfoBatch(authorWpIds);
+
+    }).then(function (users) {
+
+        /*
+         * Add user "display names" to the final result
+         */
+
+        for(userId in users) {
+            result.result.push({
+                name: users[userId].display_name,
+                wordpressId: parseInt(userId)
+            });
+            result.count++;
+        }
+
+        /*
+         * Sort final result in case-insensitive fashion
+         */
+
+        result.result.sort(function (a, b) {
+
+            if (a.name.toLowerCase() < b.name.toLowerCase()) {
+                return -1;
+            }
+
+            if (a.name.toLowerCase() > b.name.toLowerCase()) {
+                return 1;
+            }
+
+            return 0;
+        });
+
+    }).then(function () {
+
+        return res.status(200).json(result);
+
+    }).catch(function (error) {
+
+        if (!error.error) {
+            error.error = { status: 500 };
+        }
+        if (!error.error.status) {
+            error.error.status = 500;
+        }
+        return res.status(error.error.status).json(error);
+
+    });
 });
 
 module.exports = router;
