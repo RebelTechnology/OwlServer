903eb1b0199bcc1b360f7d022521ba38c7d30a29
discopatrick <git@discopatrick.me>
2016-03-10 17:59:17 +0000

    only show download link if there is a url to link to

--- web/wordpress/wp-content/themes/hoxton-owl-2014/page-patch-library/js/patchManager.js
+++ web/wordpress/wp-content/themes/hoxton-owl-2014/page-patch-library/js/patchManager.js
@@ -490,14 +490,19 @@ HoxtonOwl.patchManager = {
 
                             cnt++;
 
-                            var downloadLink = url;
-                            if (actuallyFromGitHub) {
-                                downloadLink = downloadLink.replace('github.com', 'raw.githubusercontent.com');
-                                downloadLink = downloadLink.replace('/blob', '');
+                            if (url)
+                            {
+                                var downloadLink = url;
+                                if (actuallyFromGitHub) {
+                                    downloadLink = downloadLink.replace('github.com', 'raw.githubusercontent.com');
+                                    downloadLink = downloadLink.replace('/blob', '');
+                                }
                             }
 
+
                             $('#github-files > ul').append('<li><a href="#tabs-' + cnt + '">' + filename + '</a>' + 
-                                '<a target="_blank" href="' + downloadLink + '"><img src="/wp-content/themes/hoxton-owl-2014/page-patch-library/images/download.png"/></a></li>');
+                                (downloadLink ? '<a target="_blank" href="' + downloadLink + '"><img src="/wp-content/themes/hoxton-owl-2014/page-patch-library/images/download.png"/></a>' : '') +
+                                '</li>');
                             $('#github-files').append('<div id="tabs-' + cnt + '"><pre class="prettyprint"></pre></div>');
                             if (actuallyFromGitHub) {
                                 $('#github-files #tabs-' + cnt).prepend('<a href="' + url + '" target="_new" class="github-link">Open this file in GitHub</a>');
