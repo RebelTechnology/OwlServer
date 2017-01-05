18c7f1b6f173f0936f4dde06296e59d127659f87
Sam Artuso <sam@highoctanedev.co.uk>
2017-01-05 18:08:15 +0000

    Reformatting.

--- web/api/lib/wordpress-bridge.js
+++ web/api/lib/wordpress-bridge.js
@@ -2,53 +2,56 @@
  * @author Sam Artuso <sam@highoctanedev.co.uk>
  */
 
-var wordpress = require('wordpress');
-var apiSettings = require('../api-settings');
+const wordpress = require('wordpress');
+const apiSettings = require('../api-settings');
 
-var client = wordpress.createClient({
-    url: apiSettings.WORDPRESS_XML_RPC_ENDPOINT,
-    username: apiSettings.WORDPRESS_XML_RPC_USERNAME,
-    password: apiSettings.WORDPRESS_XML_RPC_PASSWORD
+const client = wordpress.createClient({
+  url: apiSettings.WORDPRESS_XML_RPC_ENDPOINT,
+  username: apiSettings.WORDPRESS_XML_RPC_USERNAME,
+  password: apiSettings.WORDPRESS_XML_RPC_PASSWORD,
 });
 
 /**
  * Tries to validate a user cookie.
  */
-module.exports.validateAuthCookie = function(cookie, callback) {
-
-    client.call('owl.validateAuthCookie', cookie, function (error, data) {
-        if (error) {
-            callback(error);
-        } else {
-            callback(null, data);
-        }
-    });
+const validateAuthCookie = function (cookie, callback) {
+  client.call('owl.validateAuthCookie', cookie, (error, data) => {
+    if (error) {
+      callback(error);
+    } else {
+      callback(null, data);
+    }
+  });
 };
 
 /**
  * Returns information about a WP user.
  */
-module.exports.getUserInfo = function (username, callback) {
-
-    client.call('owl.getUserInfo', username, function (error, data) {
-        if (error) {
-            callback(error);
-        } else {
-            callback(null, data);
-        }
-    });
+const getUserInfo = function (username, callback) {
+  client.call('owl.getUserInfo', username, (error, data) => {
+    if (error) {
+      callback(error);
+    } else {
+      callback(null, data);
+    }
+  });
 };
 
 /**
  * Returns information about the WP users identified by the given user IDs.
  */
-module.exports.getUserInfoBatch = function (userIds, callback) {
+const getUserInfoBatch = function (userIds, callback) {
+  client.call('owl.getUserInfoBatch', userIds, (error, data) => {
+    if (error) {
+      callback(error);
+    } else {
+      callback(null, data);
+    }
+  });
+};
 
-    client.call('owl.getUserInfoBatch', userIds, function (error, data) {
-        if (error) {
-            callback(error);
-        } else {
-            callback(null, data);
-        }
-    });
+module.exports = {
+  validateAuthCookie,
+  getUserInfo,
+  getUserInfoBatch,
 };
