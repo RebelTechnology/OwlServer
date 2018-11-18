f2f1be49ebdc7a62d0e527c34ea9730860922f94
goozo <2273618+goozo@users.noreply.github.com>
2018-11-18 15:14:50 +0100

    updatelogo url

--- web/wordpress/wp-content/themes/shopkeeper-child/functions.php
+++ web/wordpress/wp-content/themes/shopkeeper-child/functions.php
@@ -46,7 +46,7 @@
   function login_logo() { ?>
     <style type="text/css">
         #login h1 a, .login h1 a {
-          background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/site-login-logo.png);
+          background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/logo.png);
           height: 65px;
           width: 320px;
           background-size: 320px 65px;
