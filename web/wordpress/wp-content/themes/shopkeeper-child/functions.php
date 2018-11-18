0b3bb07f4dc3db9b6918bdc10ca2031713799f38
goozo <2273618+goozo@users.noreply.github.com>
2018-11-18 15:22:33 +0100

    adds child theme login style sheet

--- web/wordpress/wp-content/themes/shopkeeper-child/functions.php
+++ web/wordpress/wp-content/themes/shopkeeper-child/functions.php
@@ -43,16 +43,8 @@
 
   add_action('wp_enqueue_scripts', 'registerSpaScripts');
 
-  function login_logo() { ?>
-    <style type="text/css">
-        #login h1 a, .login h1 a {
-          background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/logo.png);
-          height: 65px;
-          width: 320px;
-          background-size: 320px 65px;
-          background-repeat: no-repeat;
-          padding-bottom: 30px;
-        }
-    </style>
-<?php }
-add_action( 'login_enqueue_scripts', 'login_logo' );
+  function wpse_login_styles() {
+    wp_enqueue_style( 'wpse-custom-login', get_stylesheet_directory_uri() . '/style-login.css' );
+  }
+
+  add_action( 'login_enqueue_scripts', 'wpse_login_styles' );
