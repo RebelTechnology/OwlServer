27cd8b34020d75000560bfa21311499e512c8cbe
goozo <2273618+goozo@users.noreply.github.com>
2018-11-18 15:14:06 +0100

    custom login logo

--- web/wordpress/wp-content/themes/shopkeeper-child/functions.php
+++ web/wordpress/wp-content/themes/shopkeeper-child/functions.php
@@ -42,3 +42,17 @@
   }
 
   add_action('wp_enqueue_scripts', 'registerSpaScripts');
+
+  function login_logo() { ?>
+    <style type="text/css">
+        #login h1 a, .login h1 a {
+          background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/site-login-logo.png);
+          height: 65px;
+          width: 320px;
+          background-size: 320px 65px;
+          background-repeat: no-repeat;
+          padding-bottom: 30px;
+        }
+    </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'login_logo' );
