900475b8b89016bb73942e010e09b6d37f1bb187
fabian <fabian@rebeltech.org>
2021-12-04 16:18:19 +0100

    a comma

--- web/owlspa/src/containers/OwlControl/OwlControl.js
+++ web/owlspa/src/containers/OwlControl/OwlControl.js
@@ -61,7 +61,7 @@ class OwlControl extends Component {
       patchIsLoading,
       patchIsStoring,
       presets,
-      activePresetSlot
+      activePresetSlot,
       uuid,
     } = owlState;
 
